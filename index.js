5785b5cda1be8eac396b7e88179e5c39938de494
clement <116070495+ClementMbai@users.noreply.github.com>
2025-03-18 20:23:51 +0300

    work

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-function sleep(){
-    console.log("restores energy");
+/*
+let tasks = [];
+function addTask() {
+  const input = document.querySelector("#task-input").value;
+  if (input) {
+    tasks.push(input);
+    const list = document.querySelector("#task-list");
+    list.innerHTML = ""; // Clear and rebuild
+    for (let task of tasks) {
+      list.innerHTML += `<li>${task}</li>`;
+    }
+    document.querySelector("#task-input").value = ""; // Reset input
+  }
 }
+*/
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let fruit = fruits[fruits.length - 1];
+console.log(fruit);
+
+
+
+
+
+
+
